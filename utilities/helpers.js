3bfcba41e5770df806f0655033b129f3fcbae4e5
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-08-19 09:23:25 +0100

    auto renewal

--- utilities/helpers.js
+++ utilities/helpers.js
@@ -25,10 +25,7 @@ const checkLoans = async()=>{
     let additionalFee;
     for (let i = 0; i < loans.length; i++) {
       let maximumDate = loans[i].dueDate;
-      console.log(currentDate)
-      console.log(currentDate>maximumDate);
-      console.log(maximumDate)
-      if(currentDate>=maximumDate){
+      if(currentDate>maximumDate){
         loanCategory = await models.loanCategory.findOne(
           {
             where:{
@@ -53,6 +50,115 @@ const checkLoans = async()=>{
           }
         );
       }
+    }
+  }
+}
+const checkInvestment = async (req,res)=>{
+  let currentDate = new Date();
+  let investments = await models.investment.findAll(
+    {
+      where:{
+        status:true,
+        isRedemmed:false
+      }
+    }
+  );
+  if(investments){
+    let investmentPlan,wallet,user,dueDate;
+    for (let i = 0; i < investments.length; i++) {
+      dueDate = investments[i].dueDate
+      if(currentDate>dueDate){
+        user = await models.user.findOne(
+          {
+            where:{
+              id:investments[i].userId
+            }
+          }
+        );
+        wallet = await models.wallet.findOne(
+          {
+            where:{
+              userId:user.id
+            }
+          }
+        );
+        await models.wallet.update(
+          {
+            accountBalance:parseFloat(wallet.accountBalance) + parseFloat(investments[i].payout)
+          },
+          {
+            where:{
+              userId:user.id
+            }
+          }
+        );
+        await models.investment.update(
+          {
+            isRedemmed:true
+          },
+          {
+            where:{
+              id:investments[i].id
+            }
+          }
+        );
+        if(investments[i].autoRenewal){
+          let creditCard = null;
+            creditCard = await models.creditCard.findOne(
+              {
+                where:{
+                  isDefault:true
+                }
+              }
+            );
+          if(!creditCard) {
+            creditCard = await models.creditCard.findOne(
+              {
+                where:{
+                  userId:user.id
+                }
+              }
+            )
+          }
+          if(creditCard){
+            let digits = helpers.generateOTP()
+            let name = user.firstName;
+            let firstDigit = name.substring(0,1);
+            let trxRef = `INVESTMENT-${digits}${firstDigit}`
+            let time = new Date();
+            time = time.toLocaleString();
+            investmentPlan = await models.investmentCategoryId.findOne(
+              {
+                where:{
+                  id:investments[i].investmentCategoryId
+                }
+              }
+            );
+            let unit = amount / parseFloat(investmentPlan.pricePerUnit);
+            let interestAmount = parseFloat(investmentPlan.interestRate) * amount;
+            let payout = amount + interestAmount;
+            Date.prototype.addDays = function(days) {
+              var date = new Date(this.valueOf());
+              date.setDate(date.getDate() + days);
+              return date;
+            };
+            let date = new Date();
+            date = date.addDays(parseFloat(investmentPlan.period));
+            const createInvestment = await models.investment.create(
+              {
+                payout:payout,
+                unit:unit,
+                investmentCategoryId:investmentPlan.id,
+                userId:user.id,
+                autoRenewal:data.isAutoRenewal,
+                dueDate:date,
+                isRedemmed:false,
+                status:false
+              }
+            );
+          }
+        }
+      } 
     }
   }
 }
