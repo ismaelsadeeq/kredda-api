b2f2c3574deb7ae6582ac6389acd9267fdc7483f
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-08-21 15:49:19 +0100

    baxi api

--- utilities/baxi.api.js
+++ utilities/baxi.api.js
@@ -156,7 +156,7 @@ const purchaseData = async (payload,res)=>{
   var request = require('request');
   var options = {
     'method': 'POST',
-    'url': `https://payments.baxipay.com.ng/api/baxipay​/services​/databundle​/bundles`,
+    'url': `https://payments.baxipay.com.ng/api/baxipay​/services​/databundle​/request`,
     'headers': {
       'Content-Type': 'application/json',
       'Authorization':`Api-key ${process.env.BAXI_KEY}`,
@@ -217,6 +217,343 @@ const purchaseData = async (payload,res)=>{
     return res.json(responseData)
   });
 }
+const getCableBouquets = async (req,res)=>{
+  var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://payments.baxipay.com.ng/api/baxipay/services​/databundle​/bundles`,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization':`Api-key ${process.env.BAXI_KEY}`,
+      'Baxi-date': new Date()
+    },
+    body:{
+      service_type:payload.type
+    }
+  };
+  request(options,async function (error, response) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(response.body);
+    console.log(data)
+    if(data.code ==200 && data.status=="success"){
+      res.statusCode = 200;
+      responseData.message = "completed";
+      responseData.status = true;
+      responseData.data = data;
+      return res.json(responseData)
+    }
+    res.statusCode = 200;
+    responseData.message = "completed";
+    responseData.status = false;
+    responseData.data = data;
+    return res.json(responseData)
+  });
+}
+const getCableBouquetsAddOn = async (req,res)=>{
+  var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://payments.baxipay.com.ng/api/baxipay/services/multichoice/addons`,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization':`Api-key ${process.env.BAXI_KEY}`,
+      'Baxi-date': new Date()
+    },
+    body:{
+      product_code:payload.productCode,
+      service_type:payload.type
+    }
+  };
+  request(options,async function (error, response) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(response.body);
+    console.log(data)
+    if(data.code ==200 && data.status=="success"){
+      res.statusCode = 200;
+      responseData.message = "completed";
+      responseData.status = true;
+      responseData.data = data;
+      return res.json(responseData)
+    }
+    res.statusCode = 200;
+    responseData.message = "completed";
+    responseData.status = false;
+    responseData.data = data;
+    return res.json(responseData)
+  });
+}
+const purchaseCableTv = async (payload,res)=>{
+  var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://payments.baxipay.com.ng/api/baxipay/services/multichoice/request`,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization':`Api-key ${process.env.BAXI_KEY}`,
+      'Baxi-date':new Date()
+    },
+    body:{
+      agentReference:payload.reference,
+      smartcard_number:payload.cardNo,
+      product_monthsPaidFor:payload.productMonthsPaidFor,
+      addon_monthsPaidFor:payload.addonMonthsPaidFor,
+      addon_code:addonCode,
+      agentId:process.env.agentId,
+      product_code:payload.productCode,
+      service_type:payload.type,
+      total_amount:payload.amount
+    }
+  };
+  request(options,async function (error, response) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(response.body);
+    console.log(data)
+    if(data.code ==200 && data.status=="success"){
+      const  createTransaction = await models.serviceTransaction.create(
+        {
+          id:uuid.v4(),
+          userId:payload.userId,
+          serviceId:payload.serviceId,
+          reference:payload.reference,
+          amount:payload.amount,
+          status:"successful",
+          beneficiary:payload.cardNo,
+          time:time,
+          totalServiceFee:payload.totalServiceFee,
+          profit:payload.profit
+        }
+      );
+      res.statusCode = 200;
+      responseData.message = "completed";
+      responseData.status = true;
+      responseData.data = data;
+      return res.json(responseData)
+    }
+    const  createTransaction = await models.serviceTransaction.create(
+      {
+        id:uuid.v4(),
+        userId:payload.userId,
+        serviceId:payload.serviceId,
+        reference:payload.reference,
+        amount:payload.amount,
+        beneficiary:payload.cardNo,
+        time:time,
+        status:"failed",
+        totalServiceFee:payload.totalServiceFee,
+        profit:payload.profit
+      }
+    );
+    res.statusCode = 200;
+    responseData.message = "completed";
+    responseData.status = false;
+    responseData.data = data;
+    return res.json(responseData)
+  });
+}
+const getEpinBundles = async (req,res)=>{
+  var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://payments.baxipay.com.ng/api/baxipay/services/epin/bundles`,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization':`Api-key ${process.env.BAXI_KEY}`,
+      'Baxi-date': new Date()
+    },
+    body:{
+      service_type:payload.type
+    }
+  };
+  request(options,async function (error, response) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(response.body);
+    console.log(data)
+    if(data.code ==200 && data.status=="success"){
+      res.statusCode = 200;
+      responseData.message = "completed";
+      responseData.status = true;
+      responseData.data = data;
+      return res.json(responseData)
+    }
+    res.statusCode = 200;
+    responseData.message = "completed";
+    responseData.status = false;
+    responseData.data = data;
+    return res.json(responseData)
+  });
+}
+const purchaseWaecDirectPin = async (payload,res)=>{
+  var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://payments.baxipay.com.ng/api/baxipay/services​/epin​/request`,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization':`Api-key ${process.env.BAXI_KEY}`,
+      'Baxi-date':new Date()
+    },
+    body:{
+      agentReference:payload.reference,
+      agentId:process.env.agentId,
+      pinValue:payload.pinValue,
+      numberOfPins:payload.numberOfPins,
+      service_type:payload.type,
+      amount:payload.amount,
+      phone:payload.phoneNumber
+    }
+  };
+  request(options,async function (error, response) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(response.body);
+    console.log(data)
+    if(data.code ==200 && data.status=="success"){
+      const  createTransaction = await models.serviceTransaction.create(
+        {
+          id:uuid.v4(),
+          userId:payload.userId,
+          serviceId:payload.serviceId,
+          reference:payload.reference,
+          amount:payload.amount,
+          status:"successful",
+          beneficiary:payload.phoneNumber,
+          time:time,
+          totalServiceFee:payload.totalServiceFee,
+          profit:payload.profit
+        }
+      );
+      res.statusCode = 200;
+      responseData.message = "completed";
+      responseData.status = true;
+      responseData.data = data;
+      return res.json(responseData)
+    }
+    const  createTransaction = await models.serviceTransaction.create(
+      {
+        id:uuid.v4(),
+        userId:payload.userId,
+        serviceId:payload.serviceId,
+        reference:payload.reference,
+        amount:payload.amount,
+        beneficiary:payload.phoneNumber,
+        time:time,
+        status:"failed",
+        totalServiceFee:payload.totalServiceFee,
+        profit:payload.profit
+      }
+    );
+    res.statusCode = 200;
+    responseData.message = "completed";
+    responseData.status = false;
+    responseData.data = data;
+    return res.json(responseData)
+  });
+}
+const getAvailableElectricityBillers = async (req,res)=>{
+  var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://payments.baxipay.com.ng/api/baxipay/services​/electricity​/billers`,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization':`Api-key ${process.env.BAXI_KEY}`
+    },
+    body:{
+      service_type:payload.type
+    }
+  };
+  request(options,async function (error, response) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(response.body);
+    console.log(data)
+    if(data.code ==200 && data.status=="success"){
+      res.statusCode = 200;
+      responseData.message = "completed";
+      responseData.status = true;
+      responseData.data = data;
+      return res.json(responseData)
+    }
+    res.statusCode = 200;
+    responseData.message = "completed";
+    responseData.status = false;
+    responseData.data = data;
+    return res.json(responseData)
+  });
+}
+const purchaseElectricity = async (payload,res)=>{
+  var request = require('request');
+  var options = {
+    'method': 'POST',
+    'url': `https://payments.baxipay.com.ng/api/baxipay/services/electricity/request`,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization':`Api-key ${process.env.BAXI_KEY}`,
+      'Baxi-date':new Date()
+    },
+    body:{
+      agentReference:payload.reference,
+      agentId:process.env.agentId,
+      account_number:payload.meterNo,
+      service_type:payload.type,
+      amount:payload.amount,
+      phone:payload.phoneNumber
+    }
+  };
+  request(options,async function (error, response) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(response.body);
+    console.log(data)
+    if(data.code ==200 && data.status=="success"){
+      const  createTransaction = await models.serviceTransaction.create(
+        {
+          id:uuid.v4(),
+          userId:payload.userId,
+          serviceId:payload.serviceId,
+          reference:payload.reference,
+          amount:payload.amount,
+          status:"successful",
+          beneficiary:payload.meterNo,
+          time:time,
+          totalServiceFee:payload.totalServiceFee,
+          profit:payload.profit
+        }
+      );
+      res.statusCode = 200;
+      responseData.message = "completed";
+      responseData.status = true;
+      responseData.data = data;
+      return res.json(responseData)
+    }
+    const  createTransaction = await models.serviceTransaction.create(
+      {
+        id:uuid.v4(),
+        userId:payload.userId,
+        serviceId:payload.serviceId,
+        reference:payload.reference,
+        amount:payload.amount,
+        beneficiary:payload.meterNo,
+        time:time,
+        status:"failed",
+        totalServiceFee:payload.totalServiceFee,
+        profit:payload.profit
+      }
+    );
+    res.statusCode = 200;
+    responseData.message = "completed";
+    responseData.status = false;
+    responseData.data = data;
+    return res.json(responseData)
+  });
+}
 module.exports = {
-  queryTransaction
+  queryTransaction,
+  purchaseAirtime,
+  getDataBundles,
+  purchaseData,
+  getCableBouquets,
+  getCableBouquetsAddOn,
+  purchaseCableTv,
+  getEpinBundles,
+  purchaseWaecDirectPin,
+  getAvailableElectricityBillers,
+  purchaseElectricity
 }
